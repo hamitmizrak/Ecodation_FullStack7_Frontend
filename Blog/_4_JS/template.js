253a35bbc33ee70ff49df75213cd6317ba8a85e9
hamitmizrak <hamitmizrak@gmail.com>
2023-01-21 11:12:49 +0300

    değişkenklerde

---
+++ Blog/_4_JS/template.js
@@ -0,0 +1,39 @@
+// alert("Denememe");
+// window.alert("window.alert");
+// document.write("data yazdıldı");
+
+// interpreter nedir ?
+// JS senkron yapı sistemi nedir ?
+// JS asenkron yapabilmemiz için neler yapmamız gerekiyor. 
+
+// JS: ister tek tırnak ister çift tırnak kullanabilirsiniz
+// JS: case sensitive değildir.
+
+// JS: client side ( Browser olmadan çalıştırmak ): Frontend
+// JS: server side ( Node js ) : Backend
+
+// single quote (Tek tırnak)   ==> ''
+// double quote (Çift tırnak)  ==> ""
+// backtick                    ==> ``
+
+// Hyphen       -
+// camelCaseValue   
+// PascalCaseValue
+
+// console.log("Loglama");
+// console.warn("Warning");
+// console.error("error");
+//console.info("info");
+
+//Variable 
+// isim sıfat kullanabilirsiniz.
+//Global değişkendir.
+var _$degiskenAdi44="Kastamonu";
+console.log(_$degiskenAdi44);
+//değişkenlerde
+//sayı ile başlanmaz
+//$ ve  _ haricinde özel karakter veremeyiz.
+//üğşçöİ  vermeyelim.
+
+//let      //ES6
+//const   //ES6
